0248f441ef4e5f979df2aa116f5d4bd0f29ca553
Kshitij <kshitij.tanwar2002@gmail.com>
2023-09-21 00:46:54 +0530

    first commit

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,9 +1,10 @@
 import "./News.css";
+import placeholder from "../../assets/placeholder-news.jpg"
 const News = ({img, title, id, publishDate, description }) => {
     return (
         <div className="news-card" key={id}>
             <div className="image-sec">
-                <img src={img} alt="not found" />
+                <img src={img?img:placeholder} alt="not found" />
             </div>
             <div>
             <h3>{title}</h3>
