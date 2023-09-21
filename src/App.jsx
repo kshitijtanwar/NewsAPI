8c29235b38e506ba21cba76b56a274d6dead5316
Kshitij <kshitij.tanwar2002@gmail.com>
2023-09-22 00:56:19 +0530

    .finally for isLoading

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,58 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import NewsList from "./components/NewsList/NewsList";
 
 function App() {
     const [news, setNews] = useState([]);
-    const [isLoading,setIsLoading] = useState(false);
-    const [error,setError] = useState("");
-    const fetchNewsHandler = () => {
-      setIsLoading(true);
-      fetch(
-        "https://newsapi.org/v2/top-headlines?country=in&apiKey=52d12c5185e4497ba95d820df1a1454e"
-        )
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          const transformedNews = data.articles.map((news) => {
-            return {
-              id:Math.random(),
-              description: news.description,
-              title: news.title,
-              publishDate: news.publishedAt,
-              img:news.urlToImage
-            };
-          });
-          setNews(transformedNews);
-          setIsLoading(false);
-            }).catch(error =>{
-              setError(error.message);
-            });
-            setIsLoading(false);
-    };
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState("");
 
-    let content = <p>No News Found!!</p>;
-    if(news.length > 0){
-      content = <NewsList news={news} />;
+    const fetchNewsHandler = useCallback(async () => {
+        setIsLoading(true);
+        setError(null);
+        fetch(
+            "https://newsapi.org/v2/top-headlines?country=in&apiKey=52d12c5185e4497ba95d820df1a1454e"
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                const transformedNews = data.articles.map((news) => {
+                    return {
+                        id: Math.random(),
+                        description: news.description,
+                        title: news.title,
+                        publishDate: news.publishedAt,
+                        img: news.urlToImage,
+                    };
+                });
+                setNews(transformedNews);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                setError(error.message);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }, []);
+
+    useEffect(() => {
+        fetchNewsHandler();
+    }, [fetchNewsHandler]);
+
+    let content = <p></p>;
+    if (news.length > 0) {
+        content = <NewsList news={news} />;
     }
-    if(isLoading){
-      content = <p>Please wait loading...</p>
+    if (isLoading) {
+        content = <p>Please wait loading...</p>;
     }
-    if(error){
-      content = <p>Something went wrong...</p>
+    if (error) {
+        content = <p>Something went wrong...</p>;
     }
 
     return (
